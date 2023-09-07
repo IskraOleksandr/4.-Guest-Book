f649ce2fd8c176c52120996e16636fdb70107b60
_Iskra_ <_Iskra_@DESKTOP-4SUHQ4F>
2023-09-07 10:40:08 +0300

    #Task6 Добавление контроллера UserController с методами

---
+++ Гостевая книга Guest Book/Controllers/UserController.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+using System.Text;
+using Гостевая_книга_Guest_Book.Models;
+
+namespace Гостевая_книга_Guest_Book.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly Guest_BookContext _context;
+
+        public UserController(Guest_BookContext context)
+        {
+            _context = context;
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(LoginModel logon)
+        {
+            if (ModelState.IsValid)
+            {
+                if (_context.Users.ToList().Count == 0)
+                {
+                    ModelState.AddModelError("", "Wrong login or password!");
+                    return View(logon);
+                }
+                var users = _context.Users.Where(a => a.Login == logon.Login);
+                if (users.ToList().Count == 0)
+                {
+                    ModelState.AddModelError("", "Wrong login or password!");
+                    return View(logon);
+                }
+                var user = users.First();
+                string? salt = user.Salt;
+
+                byte[] password = Encoding.Unicode.GetBytes(salt + logon.Password);//переводим пароль в байт-массив  
+
+                var md5 = MD5.Create();//создаем объект для получения средств шифрования  
+
+                byte[] byteHash = md5.ComputeHash(password);//вычисляем хеш-представление в байтах  
+
+                StringBuilder hash = new StringBuilder(byteHash.Length);
+                for (int i = 0; i < byteHash.Length; i++)
+                    hash.Append(string.Format("{0:X2}", byteHash[i]));
+
+                if (user.Password != hash.ToString())
+                {
+                    ModelState.AddModelError("", "Wrong login or password!");
+                    return View(logon);
+                }
+                HttpContext.Session.SetString("Login", user.Login);
+
+                return RedirectToAction("Index", "Message");
+            }
+            return View(logon);
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register(RegisterModel reg)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = new User();
+                user.Login = reg.Login;
+
+                byte[] saltbuf = new byte[16];
+
+                RandomNumberGenerator randomNumberGenerator = RandomNumberGenerator.Create();
+                randomNumberGenerator.GetBytes(saltbuf);
+
+                StringBuilder sb = new StringBuilder(16);
+                for (int i = 0; i < 16; i++)
+                    sb.Append(string.Format("{0:X2}", saltbuf[i]));
+                string salt = sb.ToString();
+
+                byte[] password = Encoding.Unicode.GetBytes(salt + reg.Password);//переводим пароль в байт-массив  
+
+                var md5 = MD5.Create();//создаем объект для получения средств шифрования  
+
+                byte[] byteHash = md5.ComputeHash(password);//вычисляем хеш-представление в байтах  
+
+                StringBuilder hash = new StringBuilder(byteHash.Length);
+                for (int i = 0; i < byteHash.Length; i++)
+                    hash.Append(string.Format("{0:X2}", byteHash[i]));
+
+                user.Password = hash.ToString();
+                user.Salt = salt;
+                _context.Users.Add(user);
+                _context.SaveChanges();
+                return RedirectToAction("Login");
+            }
+
+            return View(reg);
+        }
+    }
+}
