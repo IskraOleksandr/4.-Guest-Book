ba31c7a53ab450ed6d33487e6a39bc7f57511fa1
_Iskra_ <_Iskra_@DESKTOP-4SUHQ4F>
2023-09-07 10:15:38 +0300

    #Task4 Добавление класса контекста Guest_BookContext

---
+++ Гостевая книга Guest Book/Models/Guest_BookContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Гостевая_книга_Guest_Book.Models
+{
+    public class Guest_BookContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public Guest_BookContext(DbContextOptions<Guest_BookContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
