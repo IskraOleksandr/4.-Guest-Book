632dfb3043f827e1749551d175095a97fea4774e
_Iskra_ <_Iskra_@DESKTOP-4SUHQ4F>
2023-09-07 10:54:03 +0300

    #Task9 Изменение файлаов Program.cs и appsettings.json

--- Гостевая книга Guest Book/Program.cs
+++ Гостевая книга Guest Book/Program.cs
@@ -1,27 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using Гостевая_книга_Guest_Book.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Все сессии работают поверх объекта IDistributedCache, и 
+// ASP.NET Core предоставляет встроенную реализацию IDistributedCache
+builder.Services.AddDistributedMemoryCache();// добавляем IDistributedMemoryCache
+builder.Services.AddSession();  // Добавляем сервисы сессии
+
+// Получаем строку подключения из файла конфигурации
+string? connection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// добавляем контекст ApplicationContext в качестве сервиса в приложение
+builder.Services.AddDbContext<Guest_BookContext>(options => options.UseSqlServer(connection));
+
+// Добавляем сервисы MVC
 builder.Services.AddControllersWithViews();
 
+
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
+app.UseSession();   // Добавляем middleware-компонент для работы с сессиями
+app.UseStaticFiles(); // обрабатывает запросы к файлам в папке wwwroot
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Message}/{action=Index}/{id?}");
 
 app.Run();
