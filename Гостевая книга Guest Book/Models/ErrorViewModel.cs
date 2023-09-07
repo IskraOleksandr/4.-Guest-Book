c2acb31ae2cbd44010d3de04870da809fb05bbcf
_Iskra_ <_Iskra_@DESKTOP-4SUHQ4F>
2023-09-07 09:58:51 +0300

    Добавьте файлы проекта.

---
+++ Гостевая книга Guest Book/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Гостевая_книга_Guest_Book.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
